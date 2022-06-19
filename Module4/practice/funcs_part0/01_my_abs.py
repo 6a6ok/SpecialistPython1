fe3e6547630d411df250ff3f42dd834e21e8f7df
Aleksandr Shashkov <105604550+6a6ok@users.noreply.github.com>
2022-06-19 14:57:05 +0300

    Update 01_my_abs.py

--- Module4/practice/funcs_part0/01_my_abs.py
+++ Module4/practice/funcs_part0/01_my_abs.py
@@ -1,8 +1,9 @@
-def my_abs(...):
-    ...
-    return ...
+def my_abs(a):
+    res = (a ** 2) ** 0.5
+    return res
 
 
-print(my_abs(-5))
-print(my_abs(5))
+print(my_abs(-9))
+print(my_abs(-1))
 print(my_abs(0))
+print(my_abs(-9.9))
